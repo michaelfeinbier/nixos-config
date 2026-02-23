360f5734f0422e418e230ef86718723df728b73b
Michael Feinbier <mf@tiberianmaster.net>
2026-02-23 20:10:49 +0100

    Apply NixOS generation 92

--- configurations/nixos/nixos-gaming/configuration.nix
+++ configurations/nixos/nixos-gaming/configuration.nix
@@ -17,8 +17,8 @@ in
   nixpkgs.config.allowUnfree = true;
 
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  #boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  #boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
