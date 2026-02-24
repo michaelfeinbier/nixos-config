60b2a1155459292d13c92f4b7a1a5acec904c934
Michael Feinbier <mf@tiberianmaster.net>
2026-02-24 20:50:21 +0100

    Hypridle listener

--- modules/home/services/hypridle.nix
+++ modules/home/services/hypridle.nix
@@ -1,14 +1,27 @@
 { ... }:
 {
-  
   services.hypridle = {
     enable = true;
     settings = {
       general = {
         before_sleep_cmd = "noctalia-shell ipc call lockScreen lock";
-        after_sleep_cmd = "pidof Hyprland >/dev/null && hyprctl dispatch dpms on || niri msg action power-on-monitors";
+        after_sleep_cmd = "niri msg action power-on-monitors";
         lock_cmd = "noctalia-shell ipc call lockScreen lock";
       };
+
+      listener = [
+        # Turn off monitors after 20 minutes
+        {
+          timeout = 1200;
+          on-timeout = "niri msg action power-off-monitors";
+          on-resume = "niri msg action power-on-monitors";
+        }
+        # Suspend after 60 minutes
+        {
+          timeout = 3600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
     };
   };
 }
