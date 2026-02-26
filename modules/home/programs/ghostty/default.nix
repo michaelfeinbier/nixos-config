b8552e498b2a863110823365fb24caf6712dcbb5
Michael Feinbier <mf@tiberianmaster.net>
2026-02-27 00:10:54 +0100

    Apply NixOS generation 96

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -8,9 +8,10 @@
       font-size = 13;
       theme = "noctalia";
       custom-shader = [
-        "${flake.inputs.ghostty-shaders}/retro-terminal.glsl"
-        "${flake.inputs.ghostty-shaders}/tft.glsl"
-        "${flake.inputs.ghostty-shaders}/bloom.glsl"
+        # "${flake.inputs.ghostty-shaders}/retro-terminal.glsl"
+        # "${flake.inputs.ghostty-shaders}/tft.glsl"
+        # "${flake.inputs.ghostty-shaders}/bloom.glsl"
+        "${flake.inputs.ghostty-shaders}/glitchy.glsl"
       ];
     };
   };
