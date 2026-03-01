4a411b3a13b4085766ede6eaff5f5a4453006ba2
Michael Feinbier <mf@tiberianmaster.net>
2026-03-01 14:00:17 +0100

    Disable corepack

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -3,15 +3,14 @@
   home.packages = (with pkgs; [
     opentofu
     nodenv
-    corepack
+    #corepack
     jetbrains.phpstorm
     ansible
-    #corepack_18
 
     # PHP
-    php82
+    php84
     phpstan
-  ]) ++ (with pkgs.php82Packages; [
+  ]) ++ (with pkgs.php84Packages; [
     composer
     php-cs-fixer
   ]);
