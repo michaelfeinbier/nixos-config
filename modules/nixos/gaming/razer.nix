e6d670dacf7f36f81f9fc0c86c430ab747e319ba
Michael Feinbier <mf@tiberianmaster.net>
2026-03-11 22:13:02 +0100

    Make razer less annoying

--- modules/nixos/gaming/razer.nix
+++ modules/nixos/gaming/razer.nix
@@ -6,6 +6,12 @@ in
   hardware.openrazer = {
     enable = true;
     users = [me.username];
+
+    batteryNotifier = {
+      enable = true;
+      percentage = 30;
+      frequency = 1800; # in seconds
+    };
   };
 
   environment.systemPackages = [pkgs.razergenie];
