e9d2dce0e23a34904cac0d26a858c940ccb13b10
Michael Feinbier <mf@tiberianmaster.net>
2026-03-16 23:05:37 +0100

    Styling fix

--- modules/flake/config.nix
+++ modules/flake/config.nix
@@ -1,4 +1,4 @@
-{lib, ...}:
+{lib, inputs, ...}:
 # Global configuration for this repo
 #
 # See ./modules/flake-parts/config-module.nix for schema
@@ -9,8 +9,8 @@
     email = "mf@tiberianmaster.net";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDaFHBcj6B1u0SpHEatrJ3RExPPhD88mCO9xHmF09rZQ";
     # see https://tinted-theming.github.io/tinted-gallery/
-    color = "ciapre";
+    # color = "ciapre";
     # see https://alexandrosliaskos.github.io/Awesome_Wallpapers/
-    wallpaper = "${inputs.aesthetic-wallpapers}/images/saturn-rings.jpg"
+    wallpaper = "${inputs.aesthetic-wallpapers}/images/saturn-rings.jpg";
   };
 }
