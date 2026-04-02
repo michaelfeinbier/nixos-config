e38bf0abedcf43877c2cd540658d526a3976f541
Michael Feinbier <mf@tiberianmaster.net>
2026-04-02 22:08:58 +0200

    Apply NixOS generation 10

--- modules/home/desktop/niri/default.nix
+++ modules/home/desktop/niri/default.nix
@@ -45,14 +45,14 @@ in
     };
 
     # Environment
-    environment = {
-      "DISPLAY" = ":1";
-      "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
-      "QT_QPA_PLATFORM" = "wayland";
-      "QT_WAYLAND_DISABLE_WINDOWDECORATION" = "1";
-      "XDG_SESSION_TYPE" = "wayland";
-      "XDG_CURRENT_DESKTOP" = "niri";
-    };
+    # environment = {
+    #   "DISPLAY" = ":1";
+    #   "ELECTRON_OZONE_PLATFORM_HINT" = "auto";
+    #   "QT_QPA_PLATFORM" = "wayland";
+    #   "QT_WAYLAND_DISABLE_WINDOWDECORATION" = "1";
+    #   "XDG_SESSION_TYPE" = "wayland";
+    #   "XDG_CURRENT_DESKTOP" = "niri";
+    # };
 
     # Layout
     layout = {
